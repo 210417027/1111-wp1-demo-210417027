f65c405b1394743b976ce486be59f6270fa87a43
210417027 <210417027@gms.tku.edu.tw>
2022-09-28 20:00:03 +0800

    W04-P2: Hooks Tutorial T12_xx, fix problem in T11_xx

--- src/App_xx.js
+++ src/App_xx.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import T81_xx from './tutorial/1-useState/T11_27-error-example';
+import T81_xx from './tutorial/1-useState/T12_27-useState-basics';
+
 
 const App_xx = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 11 - 210417027</h2>
+      <h2>Tutorial 12 - 210417027</h2>
       <T81_xx />
     </div>
   );
