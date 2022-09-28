98bdfbf5d4a198cf32b16e20c0060abac7236179
210417027 <210417027@gms.tku.edu.tw>
2022-09-28 21:53:41 +0800

    W04-P4: W03 menu demo, adding filter function for categories

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App_xx from './App_xx';
+import App from './App_27';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App_xx />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+
