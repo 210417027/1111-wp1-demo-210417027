1bf9dd639cd13983cbbde80c102f8d0713326565
210417027 <210417027@gms.tku.edu.tw>
2022-10-05 20:16:45 +0800

    W05-P3: T23_xx: fetch data from Github and display them

--- src/App_xx.js
+++ src/App_xx.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import T21_xx from './tutorial/2-useEffect/T21_27-useEffect-basics';
+import T23_xx from './tutorial/2-useEffect/T23_27-useEffect-fetch-data';
 
 
 const App_xx = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 21_27</h2>
-      <T21_xx />
+      <h2>Tutorial T23_27</h2>
+      <T23_xx />
     </div>
   );
 };
