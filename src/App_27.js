a3939d5cecc66b46e780681b6501e1f641c3cc5a
210417027 <210417027@gms.tku.edu.tw>
2022-10-12 20:28:26 +0800

    W06-P2: Show <Alert_xx /> and <List_xx />

--- src/App_27.js
+++ src/App_27.js
@@ -36,12 +36,19 @@ const App_27 = () => {
   <>
     <section className="section-center">
       <form className="grocery-from" onSubmit={handleSubmit}>
+        { alert.show && <Alert_27 {...alert} removeAlert={showAlert} />}
         <h3>Grocery Bud - 210417027</h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e.g.eggs" value={name} onChange={(e) => {setName(e.target.value)}}/>
           <button type="submit" className="submit-btn" >submit</button>
         </div>
       </form>
+      { list.length > 0 && (
+        <div className="grocery-container">
+          <List_27 items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   </>
   )
