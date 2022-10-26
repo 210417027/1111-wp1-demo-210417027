8782c52089e42166ce0e929de02c42607063fb80
210417027 <210417027@gms.tku.edu.tw>
2022-10-26 19:11:18 +0800

    W08-P1: a. 依學號從後往前，顯示對應的四筆 blog，結果圖片請參考 w01-p1.png; b. 顯示 filter 篩選按鈕，結果圖片請參考 w03-p1.png

--- src/App_27.js
+++ src/App_27.js
@@ -1,80 +1,95 @@
-import React, { useState, useEffect } from 'react'
-import List_27 from './components/List_27'
-import Alert_27 from './components/Alert_27'
+import React, { useState, useEffect } from 'react';
 
-const getLocalStorage = () => {
-  let list = localStorage.getItem('list');
-  if(list) {
-    return JSON.parse(localStorage.getItem('list'));
-  } else {
-    return [];
-  }
-}
+const App_xx = () => {
+  return (
+    <>
+          <section className="blogs">
+      <div className="section-title">
+        <h2>CSS Grid using breakpoints</h2>
+      </div>
+      <div className="filter-container">
+        <button type="button" className="filter-btn" data-id="all"> all </button>
+        <button type="button" className="filter-btn" data-id="lifestyle"> lifestyle </button>
+        <button type="button" className="filter-btn" data-id="travel"> travel </button></div>
 
-const App_27 = () => {
-  const [name , setName] = useState('');
-  const [list , setList] = useState(getLocalStorage());
-  const [alert, setAlert] = useState({
-    show: false,
-    msg: '',
-    type: ''
-  });
+      <div className="blogs-center">
+        <article className="blog">
+          <img
+            src="./images/photo-7.jpg"
+            alt="Coffee photo"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle</span>
+            <h3>why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <div className="item-control">
+              <a href="#">read more</a>
+              <div className='btn-container'>
+                <button type='button' className='delete-btn'> delete </button>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article className="blog">
+          <img
+            src="./images/photo-1.jpg"
+            alt="Coffee photo"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle</span>
+            <h3>why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <div className="item-control">
+              <a href="#">read more</a>
+              <div className='btn-container'>
+                <button type='button' className='delete-btn'> delete </button>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article className="blog">
+          <img
+            src="./images/photo-2.jpg"
+            alt="Coffee photo"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle</span>
+            <h3>why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <div className="item-control">
+              <a href="#">read more</a>
+              <div className='btn-container'>
+                <button type='button' className='delete-btn'> delete </button>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article className="blog">
+          <img
+            src="./images/photo-7.jpg"
+            alt="Coffee photo"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle</span>
+            <h3>why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <div className="item-control">
+              <a href="#">read more</a>
+              <div className='btn-container'>
+                <button type='button' className='delete-btn'> delete </button>
+              </div>
+            </div>
+          </div>
+        </article>
+        </div>
+    </section>
 
-  useEffect( () => {
-    localStorage.setItem('list', JSON.stringify(list));
-  }, [list]);
-
-  const showAlert = (show = false, msg = "", type = "") => {
-    setAlert({show, msg, type });
-  };
-
-  const handleSubmit = (e) => {
-     e.preventDefault();
-     if (!name){
-      showAlert(true, 'please enter value', 'danger');
-      //
-     } else {
-      showAlert(true, 'value changed', 'success');
-      const newItem = {
-        id: new Date().getTime().toString(),
-        title: name
-      };
-      setList([...list, newItem]);
-      setName('');
+    </>
+  );
 };
 
-  };
-
-  const removeItem = (id) => {
-    showAlert(true, 'item removed', 'danger');
-    setList( list.filter( (item) => item.id !== id ));
-  };
-
-  const clearList = () => {
-    showAlert(true, 'item removed', 'danger');
-    setList([]);
-  }
-
-  return (
-  <>
-    <section className="section-center">
-      <form className="grocery-from" onSubmit={handleSubmit}>
-        { alert.show && <Alert_27 {...alert} removeAlert={showAlert} />}
-        <h3>Grocery Bud - 210417027</h3>
-        <div className="form-control">
-          <input type="text" className="grocery" placeholder="e.g.eggs" value={name} onChange={(e) => {setName(e.target.value)}}/>
-          <button type="submit" className="submit-btn" >submit</button>
-        </div>
-      </form>
-      { list.length > 0 && (
-        <div className="grocery-container">
-          <List_27 items={list} removeItem= {removeItem} />
-          <button className="clear-btn" onClick={clearList}>clear items</button>
-        </div>
-      )}
-    </section>
-  </>
-  )
-}
-
-export default App_27
+export default App_xx;
