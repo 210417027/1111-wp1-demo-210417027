1ca1e7164cf20af71fa48f9c30a8520feff57c26
210417027 <210417027@gms.tku.edu.tw>
2022-10-26 19:38:11 +0800

    W08-P2: 請將 9 筆 blogs 資訊放入陣列 blogData_xx.js 中，並能套入第 1 題的 App_xx.js 中，要能顯示所有的 blogs

--- src/App_27.js
+++ src/App_27.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import data from './blogData_27.js'
 
-const App_xx = () => {
+const App_27 = () => {
+  const [blogs, setBlogs] = useState(data);
+  console.log('blogs', blogs);
   return (
     <>
           <section className="blogs">
@@ -13,16 +16,19 @@ const App_xx = () => {
         <button type="button" className="filter-btn" data-id="travel"> travel </button></div>
 
       <div className="blogs-center">
-        <article className="blog">
+        { blogs.map( (blog) => {
+          const {id, img, title, desc, category} = blog;
+          return (
+            <article className="blog">
           <img
-            src="./images/photo-7.jpg"
+            src={img}
             alt="Coffee photo"
             className="img blog-img"
           />
           <div className="blog-content">
-            <span>lifestyle</span>
-            <h3>why coffee is awesome</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <span>{category}</span>
+            <h3>{title}</h3>
+            <p>{desc}</p>
             <div className="item-control">
               <a href="#">read more</a>
               <div className='btn-container'>
@@ -31,60 +37,8 @@ const App_xx = () => {
             </div>
           </div>
         </article>
-        <article className="blog">
-          <img
-            src="./images/photo-1.jpg"
-            alt="Coffee photo"
-            className="img blog-img"
-          />
-          <div className="blog-content">
-            <span>lifestyle</span>
-            <h3>why coffee is awesome</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <div className="item-control">
-              <a href="#">read more</a>
-              <div className='btn-container'>
-                <button type='button' className='delete-btn'> delete </button>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article className="blog">
-          <img
-            src="./images/photo-2.jpg"
-            alt="Coffee photo"
-            className="img blog-img"
-          />
-          <div className="blog-content">
-            <span>lifestyle</span>
-            <h3>why coffee is awesome</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <div className="item-control">
-              <a href="#">read more</a>
-              <div className='btn-container'>
-                <button type='button' className='delete-btn'> delete </button>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article className="blog">
-          <img
-            src="./images/photo-7.jpg"
-            alt="Coffee photo"
-            className="img blog-img"
-          />
-          <div className="blog-content">
-            <span>lifestyle</span>
-            <h3>why coffee is awesome</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <div className="item-control">
-              <a href="#">read more</a>
-              <div className='btn-container'>
-                <button type='button' className='delete-btn'> delete </button>
-              </div>
-            </div>
-          </div>
-        </article>
+          )
+        })}
         </div>
     </section>
 
@@ -92,4 +46,4 @@ const App_xx = () => {
   );
 };
 
-export default App_xx;
+export default App_27;
