5a9849924a107acadd4d09ee8f97c89722e71533
210417027 <210417027@gms.tku.edu.tw>
2022-11-30 20:58:25 +0800

    W13-P2: Apply useReducer to add an item with modal alert

--- src/App_xx.js
+++ src/App_xx.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import T81_xx from './tutorial/8-useContext/T81_27-ContextAPI';
+import T6_xx from './tutorial/6-useReducer/T6_27-useReducer';
 
 
 const App_xx = () => {
   return (
     <div className='container'>
-      <h2>Tutorial T71_27</h2>
-      <T81_xx />
+      <h2>Tutorial T6_27</h2>
+      <T6_xx />
     </div>
   );
 };
---
+++ src/tutorial/6-useReducer/T6_27_reducer.js
@@ -0,0 +1,19 @@
+export const reducer_27 = (state, action) => {
+
+    if(action.type === 'ADD_ITEM'){
+        const newPeople = [...state.people, action.payload];
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: 'item added'
+        }
+    }
+    if(action.type === 'CLOSE_MODAL') {
+        return{
+            ...state,
+            isModalOpen: false,
+            modalContent: ''
+        }
+    }
+}
