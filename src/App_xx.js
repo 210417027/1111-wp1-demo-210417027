2abf1378271a03a0b3cf2261097c3051457df4ca
210417027 <210417027@gms.tku.edu.tw>
2022-11-30 19:13:32 +0800

    W13-P1: PersonContext for T82_xx

---
+++ src/App_xx.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import T81_xx from './tutorial/8-useContext/T81_27-ContextAPI';
+
+
+const App_xx = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial T71_27</h2>
+      <T81_xx />
+    </div>
+  );
+};
+
+export default App_xx;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App_27 from './App_27';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App_xx from './App_xx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App_27 />
-  </React.StrictMode>
+    <App_xx />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
